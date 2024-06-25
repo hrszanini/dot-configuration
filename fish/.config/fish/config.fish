af021f10bc3ae97c5907f9a427111f77c838f662
hrszanini <hrszanini@gmail.com>
2024-06-25 16:48:15 -0300

    Atualização de fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,5 +4,13 @@ end
 
 starship init fish | source
 
+function fish_greeting
+    set sentences \
+    "Yeah man!" \
+    "Acho que você não tá querendo mexer no terminal não..." \
+    "Que tela bonita!" \
+    "Aaah! Que delícia de terminal, cara!"
+    echo $(random choice $sentences)
+end
 
 thefuck --alias | source
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -743,12 +743,8 @@ require('lazy').setup({
     'catppuccin/nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.cmd.colorscheme 'catppuccin-mocha'
 
-      -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
   },
